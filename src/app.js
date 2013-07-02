67a3617887c78729b7a88a839975300fac56db24
Greg Gigon <greg.gigon@gmail.com>
2013-07-03 00:06:39 +0100

    More work around New Kanban controller

--- src/app.js
+++ src/app.js
@@ -1,13 +1,57 @@
+function Kanban(name, numberOfColumns) {
+	this.name = name;
+	this.numberOfColumns = numberOfColumns;
+	this.columns = {};
+	return this;
+}
+
+function KanbanRepository(){
+	return {
+		kanbansByName : {},
+		
+		add: function(kanban){
+			this.kanbansByName[kanban.name] = kanban;
+			return kanban;
+		},
+
+		all: function(){
+			return this.kanbansByName;
+		},
+
+		remove: function(kanbanName) {
+			if (this.kanbansByName[kanbanName]){
+				delete this.kanbansByName[kanbanName];
+			}
+		}
+	}
+};
+
+
+var mpkService = angular.module('mpk.service', []);
+mpkService.factory('kanbanRepository', KanbanRepository);
+
+var mpk = angular.module('mpk', ['mpk.service']);
+
 function MenuController($scope){
 	$scope.newKanbanAction = function(){
 		return false;
 	}
 }
 
-function NewKanbanController($scope){
+function NewKanbanController($scope, kanbanRepository){
 	$scope.numberOfColumns = 3;
-	$scope.createNew = function(){
-		console.log($scope.kanbanName + " - " + $scope.numberOfColumns);
+	$scope.kanbanName = '';
+
+	$scope.createNew = function(dialogId){
+		var newKanban = new Kanban($scope.kanbanName, $scope.numberOfColumns);
+		kanbanRepository.add(newKanban);
+		$(dialogId).modal('toggle');
+		$scope.kanbanName = '';
+		$scope.numberOfColumns = 3;
 		return true;
 	}
+}
+
+function KanbanController($scope) {
+
 }

a191bc233052a9a89739e5b5ea3c0fe5c7d14b5b
Greg Gigon <greg.gigon@gmail.com>
2013-07-05 16:57:17 +0100

    Added some love to the controller

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 function Kanban(name, numberOfColumns) {
 	this.name = name;
 	this.numberOfColumns = numberOfColumns;
-	this.columns = {};
+	this.columns = [];
+	return this;
+}
+
+function KanbanColumn(name){
+	this.name = name;
+	this.cards = [];
+	return this;
+}
+
+function KanbanCard(name){
+	this.name = name;
 	return this;
 }
 
@@ -22,6 +33,14 @@ function KanbanRepository(){
 			if (this.kanbansByName[kanbanName]){
 				delete this.kanbansByName[kanbanName];
 			}
+		},
+
+		save: function(){
+			
+		},
+
+		load: function(){
+			
 		}
 	}
 };
@@ -54,4 +73,14 @@ function NewKanbanController($scope, kanbanRepository){
 
 function KanbanController($scope) {
 
+}
+
+function ApplicationController($scope){
+	var kanban = new Kanban('Test kanban', 3);
+
+	kanban.columns.push(new KanbanColumn('Column 1'));
+	kanban.columns.push(new KanbanColumn('Column 2'));
+	kanban.columns.push(new KanbanColumn('Column 3'));
+
+	$scope.kanban = kanban;
 }

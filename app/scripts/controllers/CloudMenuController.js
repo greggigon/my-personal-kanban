0f3b02ee8473d9980bc7f6693f05015825d94075
Greg Gigon <greg.gigon@gmail.com>
2013-10-29 08:41:19 +0000

    Added controller and cloud service

---
+++ app/scripts/controllers/CloudMenuController.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var CloudMenuController = function($scope){
+	
+};
---
+++ app/scripts/services/cloudService.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('mpk').factory('cloudService', function() {
+	return {
+		
+	};
+});

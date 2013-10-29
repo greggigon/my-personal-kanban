fd7c167d34f98443d055503c8d16f599b429d18e
Greg Gigon <greg.gigon@gmail.com>
2013-10-29 08:49:29 +0000

    Fixed the cardDetails service.

--- app/scripts/filters/cardDetails.js
+++ app/scripts/filters/cardDetails.js
@@ -2,7 +2,7 @@
 
 angular.module('mpk').filter('cardDetails', function () {
 	return function (input) {
-		console.log(input);
+		if (input == undefined || input === '') return input;
 		return input.replace(/&#10;/g, "<br />");
 	};
 });

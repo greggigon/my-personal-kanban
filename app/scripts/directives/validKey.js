b9d64aeab83f6062f1ddb7218ef46d317e88f52f
Greg Gigon <greg.gigon@gmail.com>
2013-11-25 09:39:59 +0000

    Fixed the validation business

--- app/scripts/directives/validKey.js
+++ app/scripts/directives/validKey.js
@@ -3,22 +3,24 @@
 angular.module('mpk').directive('validKey', function ($http) {
     return {
       require: 'ngModel',
-      link: function (scope, element, attrs, ctrl, ngModel) {
-		element.bind('blur', validate);
+      link: function (scope, element, attrs, ctrl) {
+		//element.bind('blur', validate);
 		
 
         function validate() {
-			scope.$apply(function () {
-	        	var key = element.val();
-				var params = {kanbanKey: key, action: 'get'};
+        	var key = element.val();
+			var params = {kanbanKey: key, action: 'get'};
 
-				$http.jsonp('http://localhost:8080/service/kanban?callback=JSON_CALLBACK', {params: params}).success(function(data){
-					ctrl.$setValidity('validKey', data.success)
-				}).error(function(){
-					ctrl.$setValidity('validKeyUnableToVerify', false);
-				});
+			$http.jsonp('http://localhost:8080/service/kanban?callback=JSON_CALLBACK', {params: params}).success(function(data){
+				ctrl.$setValidity('validKey', data.success)
+			}).error(function(){
+				ctrl.$setValidity('validKeyUnableToVerify', false);
 			});
+			
         }
+
+		scope.$watch(attrs.ngModel, validate);
+
       }
     };
   });

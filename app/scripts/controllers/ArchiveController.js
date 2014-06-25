f5d8c818c1bbecfd4275624ec91ee5623a17bf7f
Greg Gigon <greg.gigon@gmail.com>
2014-06-25 23:43:13 +0100

    Added filter to the list

--- app/scripts/controllers/ArchiveController.js
+++ app/scripts/controllers/ArchiveController.js
@@ -1,7 +1,13 @@
 'use strict';
 
-var ArchiveController = function ($scope, $modalInstance) {
-	
+var ArchiveController = function ($scope, $modalInstance, kanban) {
+	$scope.model = { kanban: kanban};
+
+	$scope.formatDate = function(date){
+		var date = new Date(Date.parse(date));
+		return date.toUTCString();
+	};
+
 	$scope.close = function(){
 		$modalInstance.close();
 	};
---
+++ app/scripts/filters/archivebyCardName.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('mpk').filter('archiveByCardName', function () {
+	return function (archivedCards, filterValue) {
+		var filtered = [];
+		var filter = filterValue || '';
+		angular.forEach(archivedCards, function(archivedCard){
+			if (archivedCard.card.name.toLowerCase().indexOf(filter.toLowerCase()) > -1){
+				filtered.push(archivedCard);
+			}
+		});
+		return filtered;
+	};
+});
